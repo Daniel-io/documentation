dce1a2090f598fbb78f04a92bcd2b4a93251195a
Daniel Arenas <daniel.arenas.arango@gmail.com>
2026-05-11 23:29:39 -0400

    updatE

--- API/hudb/post-row.js
+++ API/hudb/post-row.js
@@ -1,7 +1,7 @@
 // https://developers.hubspot.com/docs/api-reference/latest/cms/hubdb/rows/create-row
 
 const tableID = "102859958";
-// const token = "pat-na1-5ac7a1fe..";
+// const token = "pat-na1-5ac7a1fe-a..";
 
 
 const options = {
@@ -16,17 +16,35 @@ const options = {
   })
 };
 
-fetch(`https://api.hubapi.com/cms/hubdb/2026-03/tables/${tableID}/rows`, options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
-
 
 //TODO
 const postHubDbRow = async () => {
   try {
 
-  } catch {
+    const response = await fetch(`https://api.hubapi.com/cms/hubdb/2026-03/tables/${tableID}/rows`, options);
 
+    if(!response) {
+      const errorText = await response.text();
+      throw new Error(`HTTP ${response.status} — ${errorText}`);
+    }
+
+    const data =  await response.json();
+    console.log(`HubDB row created`, data)
+
+  } catch (error) {
+    console.error("Error fetching HubDB rows:", error.message);
+    throw error;
   }
 }
+
+postHubDbRow();
+
+
+
+
+
+// Simple variant
+// fetch(`https://api.hubapi.com/cms/hubdb/2026-03/tables/${tableID}/rows`, options)
+//   .then(res => res.json())
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
